bdfdc0582a6d3de2f0f51b92a677c195813b3b15
BL4MM3D <gugonwosu@gmail.com>
2024-11-10 00:06:34 +0100

    (Week 4 commit)

---
+++ week-4/Project_2/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+fn main() 
+{
+    let mut exp = String::new();
+    let mut age = String::new();
+
+    io::stdin().read_line(&mut exp).expect("Failed to read input");
+    let exp = exp.trim();
+
+    if exp == "y"
+    {
+      println!("What is Your Age?");
+      io::stdin().read_line(&mut age).expect("Please enter a Correct age");
+      let age:i32 = age.trim().parse().expect("Your input is not an integer, Please enter a Correct age");
+
+      if age >= 40
+      {
+        println!("Your Incentive is N1,560,000");
+      }
+
+
+       if age >= 30 && age < 40
+      {
+        println!("Your Incentive is N1,480,000");
+      }
+
+        if age < 28
+      {
+        println!("Your Incentive is N1,300,000");
+      }
+    }
+
+     else if exp == "n"
+     {
+        println!("Your Incentive is N100,000");
+     }
+     else 
+     {
+        println!("Failed to read input, make sure your answer is EXACTLY y or n");
+     }
+}
